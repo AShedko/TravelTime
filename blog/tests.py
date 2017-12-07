4f31896400ff4b91ec06a28939e80f9c9fb00811
Andrey Shedko <ayshedko@yandex.ru>
2017-12-08 00:29:36 +0300

    tests and minor template fixes

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
+"This is a file with Tests, duh"
+from time import time
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from blog.models import Post
+from blog.models import Travel_group
 
-# Create your tests here.
+class PostsTestCase(TestCase):
+    def setUp(self):
+        u = User.objects.create_user('test')
+        Post(author=u,title="test_post", text="this is a test post",created_date=time())
+    def posts_are_postable(self):
+        p = Post.objects.get(title='test_post')
+        p.publish()
+
+
+class LoginTest(TestCase):
+    c = Client()
+    resp = c.post("/login/",{'username': 'test', 'password':'testtest'})
