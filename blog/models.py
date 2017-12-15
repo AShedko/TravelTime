a913596fe3a2644e4f47f3351e6b2f83a1fd871d
AShedko <ayshedko@yandex.ru>
2017-12-15 19:57:27 +0300

    fix for django 2.0

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
